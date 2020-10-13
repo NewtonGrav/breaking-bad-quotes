e7e9f1cf242a31580781c3e2719fd794fadec79f
Gonzalo <gonzalom97@outlook.com>
2020-10-13 15:29:27 -0300

    style: Contenedor y boton para obtener una frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 // API: https://breaking-bad-quotes.herokuapp.com/v1/quotes
 
+const Contenedor = styled.div`
+	display: flex;
+	align-items: center;
+	padding-top: 5rem;
+	flex-direction: column;
+`;
+
+const Button = styled.button`
+	padding: 1rem 3rem;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 2rem;
+	border: 2px solid black;
+	color: white;
+	background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+	background-size: 300px;
+`;
+
 function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
+	return (
+		<Contenedor>
+			<Button>Obtener Frase</Button>
+		</Contenedor>
+	);
 }
 
 export default App;
