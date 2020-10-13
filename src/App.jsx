ed1847d52ae4e11a1df962699e37b8b100496032
Gonzalo <gonzalom97@outlook.com>
2020-10-13 11:45:57 -0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// API: https://breaking-bad-quotes.herokuapp.com/v1/quotes
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
